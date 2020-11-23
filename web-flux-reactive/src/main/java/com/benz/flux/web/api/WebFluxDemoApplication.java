0d2cab69d82246f731e4bc3577ab7864365373fa
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-11-23 15:13:23 +0530

    web-flux-reactive intialized

---
+++ web-flux-reactive/src/main/java/com/benz/flux/web/api/WebFluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.benz.flux.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxDemoApplication.class, args);
+    }
+
+}
